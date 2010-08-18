d8581527ab1c0a770ce672b1cdd7dcbaaddecea2
Vincent Driessen <vincent@datafox.nl>
2010-08-18 14:24:30 +0200

    Change test root.

---
+++ tests/test_rst_tables.py
@@ -0,0 +1,14 @@
+import unittest
+import rst_tables
+
+class TestRSTTableFormatter(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testSomeThing(self):
+        input = "foo"
+        self.assertEquals("foo", input)
