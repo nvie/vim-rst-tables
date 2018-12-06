66586c7a6c87fcb2972ee2b088659beb2b571b04
Matthew Brett <matthew.brett@gmail.com>
2018-12-06 14:33:17 +0000

    Refactor testing

--- tests/test_rst_tables.py
+++ tests/test_rst_tables.py
@@ -1,19 +1,42 @@
+""" Run tests with
+
+pytest tests
+"""
+
+import re
+
 # Mock out the vim library
+from os.path import join as pjoin, dirname, abspath
 import sys
-sys.path = ['tests/mocks'] + sys.path
+ROOT = abspath(pjoin(dirname(__file__), '..'))
+MOD_NAME = 'rst_tables'
+VIM_PATH = pjoin(ROOT, 'ftplugin', MOD_NAME + '.vim')
+sys.path.insert(0, pjoin(ROOT, 'tests', 'mocks'))
+
 import vim
 import mock
 
-vimvar = {}
 
+VIMVAR = {'expand("<sfile>")': VIM_PATH,
+          '&encoding': 'utf-8'}
 
 def fake_eval(x):
-    global vimvar
-    return vimvar[x]
+    global VIMVAR
+    return VIMVAR[x]
 
 vim.eval = fake_eval
 vim.current = mock.Mock()
-vimvar['foo'] = 'bar'
+
+# Read Python code in rst_tables, make into a module.
+# https://stackoverflow.com/questions/5362771/how-to-load-a-module-from-code-in-a-string
+from types import ModuleType
+with open(VIM_PATH) as fobj:
+    content = fobj.read()
+python_code = re.search(r'^Python << endpython(.*)^endpython',
+                        content, flags=re.M | re.S).groups()[0]
+mod = ModuleType(MOD_NAME)
+sys.modules[MOD_NAME] = mod
+exec(python_code, mod.__dict__)
 
 # Begin normal module loading
 import os
@@ -48,44 +71,44 @@ class TestRSTTableFormatter(unittest.TestCase):
 
     def testGetBounds(self):
         self.load_fixture_in_vim('default')
-        self.assertEquals((3, 6), get_table_bounds())
+        self.assertEqual((3, 6, ''), get_table_bounds())
 
     def testGetBoundsOnBeginOfFile(self):
         self.load_fixture_in_vim('default')
         vim.current.window.cursor = (1, 0)
-        self.assertEquals((1, 1), get_table_bounds())
+        self.assertEqual((1, 1, ''), get_table_bounds())
 
     def testGetBoundsOnEndOfFile(self):
         self.load_fixture_in_vim('default')
         vim.current.window.cursor = (8, 0)
-        self.assertEquals((8, 9), get_table_bounds())
+        self.assertEqual((8, 9, ''), get_table_bounds())
 
     def testJoinSimpleRows(self):
         input_rows = [['x', 'y', 'z'], ['foo', 'bar']]
         expected = ['x\nfoo', 'y\nbar', 'z']
-        self.assertEquals(expected, join_rows(input_rows))
+        self.assertEqual(expected, join_rows(input_rows))
 
         input_rows.append(['apple', '', 'pear'])
         expected = ['x foo apple', 'y bar', 'z pear']
-        self.assertEquals(expected, join_rows(input_rows, sep=' '))
+        self.assertEqual(expected, join_rows(input_rows, sep=' '))
 
     def testPartitionRawLines(self):
-        self.assertEquals([], partition_raw_lines([]))
-        self.assertEquals([['']], partition_raw_lines(['']))
-        self.assertEquals(
+        self.assertEqual([], partition_raw_lines([]))
+        self.assertEqual([['']], partition_raw_lines(['']))
+        self.assertEqual(
                 [['foo'], ['bar']],
                 partition_raw_lines(['foo', 'bar']))
-        self.assertEquals(
+        self.assertEqual(
                 [['foo'], ['bar']],
                 partition_raw_lines(['foo', '+----+', 'bar']))
-        self.assertEquals(
+        self.assertEqual(
                 [['foo', 'bar'], ['baz']],
                 partition_raw_lines(['+-----+', 'foo', 'bar', '----', 'baz']))
 
     def testParseSimpleTable(self):
-        self.assertEquals([['x y z']], parse_table(['x y z']))
-        self.assertEquals([['x', 'y z']], parse_table(['x  y z']))
-        self.assertEquals([['x', 'y', 'z']], parse_table(['x  y          z']))
+        self.assertEqual([['x y z']], parse_table(['x y z']))
+        self.assertEqual([['x', 'y z']], parse_table(['x  y z']))
+        self.assertEqual([['x', 'y', 'z']], parse_table(['x  y          z']))
 
     def testParseTable(self):
         self.load_fixture_in_vim('default')
@@ -95,27 +118,27 @@ class TestRSTTableFormatter(unittest.TestCase):
                 ['Bar', 'Even very very long lines like these are fine, as long as you do not put in line endings here.'],
                 ['Qux', 'This is the last line.'],
                 ]
-        self.assertEquals(expected, parse_table(vim.current.buffer[2:6]))
+        self.assertEqual(expected, parse_table(vim.current.buffer[2:6]))
 
     def testParseTableUnifiesColumns(self):
         input = ['x  y', 'a  b    c', 'only one']
         expected = [['x', 'y', ''], ['a', 'b', 'c'], ['only one', '', '']]
-        self.assertEquals(expected, parse_table(input))
+        self.assertEqual(expected, parse_table(input))
 
     def testUnifyTables(self):
         input = [[' x ', '  y'], ['xxx', ' yyyy ', 'zz']]
         expected = [[' x ', '  y', ''], ['xxx', ' yyyy ', 'zz']]
-        self.assertEquals(expected, unify_table(input))
+        self.assertEqual(expected, unify_table(input))
 
     def testUnifyTablesRemovesEmptyColumns(self):
         input = [['x', '', 'y'], ['xxx', '', 'yyyy', 'zz', '         ']]
         expected = [['x', 'y', ''], ['xxx', 'yyyy', 'zz']]
-        self.assertEquals(expected, unify_table(input))
+        self.assertEqual(expected, unify_table(input))
 
     def testParseDealsWithSpacesAtLineEnd(self):
         input = ['x  y     ', 'a  b ', 'only one']
         expected = [['x', 'y'], ['a', 'b'], ['only one', '']]
-        self.assertEquals(expected, parse_table(input))
+        self.assertEqual(expected, parse_table(input))
 
     def testParseValidTable(self):
         input = ['+-----+----+',
@@ -124,7 +147,7 @@ class TestRSTTableFormatter(unittest.TestCase):
                  '| x   | y  |',
                  '+-----+----+']
         expect = [['Foo', 'Mu'], ['x', 'y']]
-        self.assertEquals(expect, parse_table(input))
+        self.assertEqual(expect, parse_table(input))
 
     def testParseCorruptedTable(self):
         input = ['+---+---------+',
@@ -135,7 +158,7 @@ class TestRSTTableFormatter(unittest.TestCase):
                  '+-----+----+']
         expect = [['Foo', 'Mu'],
                   ['x\nblah', 'This became somewhat larger\nA new line']]
-        self.assertEquals(expect, parse_table(input))
+        self.assertEqual(expect, parse_table(input))
 
         input = ['+---+---------+',
                  '| Foo | Mu                   |',
@@ -145,7 +168,7 @@ class TestRSTTableFormatter(unittest.TestCase):
                  '+-----+----+']
         expect = [['Foo', 'Mu'],
                   ['x\nblah', 'This became somewhat larger\nA new line']]
-        self.assertEquals(expect, parse_table(input))
+        self.assertEqual(expect, parse_table(input))
 
     def testParseMultiLineFields(self):
         input = """\
@@ -158,18 +181,18 @@ class TestRSTTableFormatter(unittest.TestCase):
 +-----+---------------------+""".split('\n')
         expect = [['Foo', 'Bar'],
                   ['x', 'This is a long line\nthat is spread out\nover multiple lines']]
-        self.assertEquals(expect, parse_table(input))
+        self.assertEqual(expect, parse_table(input))
 
     def testSplitRowIntoLines(self):
         input = ['Foo', 'Bar']
         expect = [['Foo', 'Bar']]
-        self.assertEquals(expect, split_row_into_lines(input))
+        self.assertEqual(expect, split_row_into_lines(input))
         input = ['One\nTwo\nThree', 'Only one']
         expect = [['One', 'Only one'], ['Two', ''], ['Three', '']]
-        self.assertEquals(expect, split_row_into_lines(input))
+        self.assertEqual(expect, split_row_into_lines(input))
         input = ['One\n\n\nThree', 'Foo\nBar']
         expect = [['One', 'Foo'], ['', 'Bar'], ['', ''], ['Three', '']]
-        self.assertEquals(expect, split_row_into_lines(input))
+        self.assertEqual(expect, split_row_into_lines(input))
 
     def testDrawMultiLineFields(self):
         input = [['Foo', 'Bar'],
@@ -182,23 +205,23 @@ class TestRSTTableFormatter(unittest.TestCase):
 |     | that is spread out  |
 |     | over multiple lines |
 +-----+---------------------+""".split('\n')
-        self.assertEquals(expect, draw_table(input))
+        self.assertEqual(expect, draw_table('', input))
 
     def testTableLine(self):
-        self.assertEquals('', table_line([], True))
-        self.assertEquals('++', table_line([0], True))
-        self.assertEquals('+++', table_line([0,0], True))
-        self.assertEquals('++-+', table_line([0,1]))
-        self.assertEquals('+===+', table_line([3], True))
-        self.assertEquals('+===+====+', table_line([3,4], True))
-        self.assertEquals('+------------------+---+--------------------+',
+        self.assertEqual('', table_line([], True))
+        self.assertEqual('++', table_line([0], True))
+        self.assertEqual('+++', table_line([0,0], True))
+        self.assertEqual('++-+', table_line([0,1]))
+        self.assertEqual('+===+', table_line([3], True))
+        self.assertEqual('+===+====+', table_line([3,4], True))
+        self.assertEqual('+------------------+---+--------------------+',
                 table_line([18,3,20]))
 
     def testGetColumnWidths(self):
-        self.assertEquals([], get_column_widths([[]]))
-        self.assertEquals([0], get_column_widths([['']]))
-        self.assertEquals([1,2,3], get_column_widths([['x','yy','zzz']]))
-        self.assertEquals([3,3,3],
+        self.assertEqual([], get_column_widths([[]]))
+        self.assertEqual([0], get_column_widths([['']]))
+        self.assertEqual([1,2,3], get_column_widths([['x','yy','zzz']]))
+        self.assertEqual([3,3,3],
                 get_column_widths(
                     [
                         ['x','y','zzz'],
@@ -207,7 +230,7 @@ class TestRSTTableFormatter(unittest.TestCase):
                     ]))
 
     def testGetColumnWidthsForMultiLineFields(self):
-        self.assertEquals([3,6],
+        self.assertEqual([3,6],
                 get_column_widths([['Foo\nBar\nQux',
                                     'This\nis\nreally\nneat!']]))
 
@@ -215,7 +238,7 @@ class TestRSTTableFormatter(unittest.TestCase):
         input = ['+----+-----+--+-------+',
                  '| xx | xxx |  | xxxxx |',
                  '+====+=====+==+=======+']
-        self.assertEquals([2, 3, 0, 5],
+        self.assertEqual([2, 3, 0, 5],
             get_column_widths_from_border_spec(input))
 
     def testPadFields(self):
@@ -228,38 +251,38 @@ class TestRSTTableFormatter(unittest.TestCase):
                  [' Crisps   ', ' Snacks ', ' Even more yummy, I tell you! ']]
         widths = get_column_widths(table)
         for input, expect in zip(table, expected_padding):
-            self.assertEquals(expect, pad_fields(input, widths))
+            self.assertEqual(expect, pad_fields(input, widths))
 
     def testReflowRowContentsWithEnoughWidth(self):
         input = ['Foo\nbar', 'This line\nis spread\nout over\nfour lines.']
         expect = ['Foo bar', 'This line is spread out over four lines.']
-        self.assertEquals(expect, reflow_row_contents(input, [99,99]))
+        self.assertEqual(expect, reflow_row_contents(input, [99,99]))
 
     def testReflowRowContentsWithWrapping(self):
         input = ['Foo\nbar', 'This line\nis spread\nout over\nfour lines.']
         expect = ['Foo bar', 'This line is spread\nout over four lines.']
-        self.assertEquals(expect, reflow_row_contents(input, [10,20]))
+        self.assertEqual(expect, reflow_row_contents(input, [10,20]))
 
         input = ['Foo\nbar', 'This line\nis spread\nout over\nfour lines.']
         expect = ['Foo bar', 'This\nline\nis\nspread\nout\nover\nfour\nlines.']
-        self.assertEquals(expect, reflow_row_contents(input, [10,6]))
+        self.assertEqual(expect, reflow_row_contents(input, [10,6]))
 
     def testReflowRowContentsWithoutRoom(self):
-        #self.assertEquals(expect, reflow_row_contents(input))
+        #self.assertEqual(expect, reflow_row_contents(input))
         pass
 
     def testDrawTable(self):
-        self.assertEquals([], draw_table([]))
-        self.assertEquals(['+--+', '|  |', '+==+'], draw_table([['']]))
-        self.assertEquals(['+-----+', '| Foo |', '+=====+'],
-                draw_table([['Foo']]))
-        self.assertEquals(
+        self.assertEqual([], draw_table('', []))
+        self.assertEqual(['+--+', '|  |', '+==+'], draw_table('', [['']]))
+        self.assertEqual(['+-----+', '| Foo |', '+=====+'],
+                draw_table('', [['Foo']]))
+        self.assertEqual(
                 ['+-----+----+',
                  '| Foo | Mu |',
                  '+=====+====+',
                  '| x   | y  |',
                  '+-----+----+'],
-                draw_table([['Foo', 'Mu'], ['x', 'y']]))
+                draw_table('', [['Foo', 'Mu'], ['x', 'y']]))
 
 
     def testCreateTable(self):
@@ -281,7 +304,7 @@ This is paragraph text *after* the table, with
 a line ending.
 """.split('\n')
         reformat_table()
-        self.assertEquals(expect, vim.current.buffer)
+        self.assertEqual(expect, vim.current.buffer)
 
     def testCreateComplexTable(self):
         raw_lines = self.read_fixture('multiline-cells')
@@ -303,7 +326,7 @@ a line ending.
 |                | habitasse platea dictumst. Phasellus pretium iaculis.         |
 +----------------+---------------------------------------------------------------+
 """.rstrip().split('\n')
-        self.assertEquals(expect, draw_table(parse_table(raw_lines)))
+        self.assertEqual(expect, draw_table('', parse_table(raw_lines)))
 
     def testReflowTable(self):
         self.load_fixture_in_vim('reflow')
@@ -329,5 +352,5 @@ This is paragraph text *after* the table, with
 a line ending.
 """.split('\n')
         reflow_table()
-        self.assertEquals(expect, vim.current.buffer)
+        self.assertEqual(expect, vim.current.buffer)
 
